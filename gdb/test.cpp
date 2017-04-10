8db825b8ba60849fb8dd421e4e366870568612b3
yudylaw <yudylaw@gmail.com>
2017-04-10 20:29:22 +0800

    add mem

--- gdb/test.cpp
+++ gdb/test.cpp
@@ -15,6 +15,20 @@ void testArray() {
     cout<<"index of 1="<<arr[1]<<", index of 11="<<arr[11]<<endl;
 }
 
+void mem() {
+    int a;
+    char b[128];
+    //bool c;
+    char* d=new char[128];
+    int e;
+
+    b[136] = '\0';
+    b[137] = '\0';
+    b[138] = '\0';
+    b[139] = '\0';
+    strcpy(d, "haha");
+}
+
 int main() {
 	cout<<"hello gdb"<<endl;
 	string name = "yudy";
@@ -23,7 +37,8 @@ int main() {
 	p->run();
 //	Person person(20, name);
 //	person.sayHello();
-	testArray();
+//	testArray();
+	mem();
 	return 0;
 }
 
